e556aaa3bc3faa6300543e6352d0e61652a936c8
Nicholas Meyers <88592644+NMeyers2899@users.noreply.github.com>
2023-09-07 10:09:28 -0700

    Added a Timer to the GameManagerBehavior

--- Assets/Scripts/Gameplay/GameManagerBehavior.cs
+++ Assets/Scripts/Gameplay/GameManagerBehavior.cs
@@ -6,9 +6,17 @@ public class GameManagerBehavior : MonoBehaviour
 {
     [Tooltip("The actor that the player controls.")]
     [SerializeField]
-    private PlayerMovementBehavior player;
+    private PlayerMovementBehavior _player;
 
     [Tooltip("The list of levels in the game.")]
     [SerializeField]
     private LevelBehavior[] _levels;
+
+    [Tooltip("The timer meant to see how fast the player completes a level.")]
+    private float _timer = 0.0f;
+
+    /// <summary>
+    /// The timer meant to see how fast the player completes a level.
+    /// </summary>
+    public float Timer { get { return _timer; } }
 }
